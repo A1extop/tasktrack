630592d05be5b311cd1cba6a071890597071c9f3
Alex Blednyh <blednykh1995@inbox.ru>
2025-06-19 20:51:36 +0300

    add: readme md

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -46,4 +47,35 @@ func main() {
 		v1.NewTaskHandler(cfg, api, usecase1)
 	}
 	Run(ctx, cfg, logger, router)
+}
+func Run(ctx context.Context, config *config.Config, logger *zap.Logger, router *gin.Engine) {
+	notifContext, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	srv := &http.Server{
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 20 * time.Second,
+		IdleTimeout:  60 * time.Second,
+		Addr:         config.App.Host + ":" + config.App.Port,
+		Handler:      router,
+	}
+
+	logger.Info("listen: " + config.App.Host + ":" + config.App.Port)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Fatal("listen: " + err.Error())
+		}
+	}()
+
+	<-notifContext.Done()
+	stop()
+	logger.Info("Shutting down graceful")
+
+	notifContext, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(notifContext); err != nil {
+		logger.Fatal("Server forced to shutdown: " + err.Error())
+	}
+
+	logger.Info("Server exiting")
 }
